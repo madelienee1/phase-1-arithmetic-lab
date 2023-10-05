a17d0af29c50887e21c0b2d1b7546687e421d558
Madeliene <madelienestiller@gmail.com>
2023-10-05 19:05:22 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+// Q1
 
-// Write your code here
+let num1 = 31; 
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+
+// Q2
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+
+// Q3
+let num4 = 5;
+let num3 = num4 * 10 + 4;
+let mod = num3 % num4;
+
+console.log(mod);
+
+
+// Q4
+
+let numbers = [5, 11, 18, 3, 5, 7, 20, 15];
+let max = Math.max(...numbers);
+
+console.log(max);
